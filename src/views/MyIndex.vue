8e9fa4e9b3bec3a48bcd792f41d5497683a704b3
rst_wuzhenjie <wuzhenjie@sz-rst.com>
2020-05-18 18:37:24 +0800

    练习vue  搭建

---
+++ src/views/MyIndex.vue
@@ -0,0 +1,404 @@
+<template>
+  <div>
+  <!--&lt;!&ndash;[if lt IE9]>-->
+ <!--<script type="text/javascript" src="images/js/html5.js"></script>-->
+  <!--<![endif]&ndash;&gt;-->
+
+  <!-- 头部开始-->
+  <div class="top">
+    <div class="topbar wrap clearfix">
+      <div class="topbar-l fl">
+        &nbsp; &nbsp;您好，欢迎来到中润泰兴新型建材有限公司！
+      </div>
+      <div class="topbar-r hidden_xs fr">
+        <a href="javascript:;">加入收藏</a>
+        <!--<a href="book.html">在线留言</a>-->
+        <a href="lxwm.html">联系我们</a>
+      </div>
+    </div>
+  </div>
+
+  <!-- PC 端头部 -->
+  <!--<header class="header hidden_xs clearfix">-->
+    <!--<div class="wrap clearfix">-->
+      <!--<div class="logo fl">-->
+        <!--<a href="index.html" class="clearfix">-->
+          <!--<div class="logoimg fl"><img src="images/logo1.png" alt="中润泰兴新型建材有限公司" width="130px" height="79px" ></div>-->
+          <!--<div class="company_name fl">-->
+            <!--<p class="company_name_title">中润泰兴新型建材有限公司</p>-->
+            <!--<p class="company_name_ps">一体化低碳建材</p>-->
+          <!--</div>-->
+        <!--</a>-->
+      <!--</div>-->
+      <!--<div class="tel fr">-->
+        <!--<p class="top_number_ps">服务热线：</p>-->
+        <!--<p class="top_number">15012652898 / 13530896586</p>-->
+      <!--</div>-->
+    <!--</div>-->
+    <!--&lt;!&ndash; pc版导航 &ndash;&gt;-->
+    <!--<nav class="nav  clearfix">-->
+      <!--<div class="menu wrap">-->
+        <!--<ul>-->
+          <!--<li class="mainlevel">-->
+            <!--<a href="onepage.html">公  司  简  介</a>-->
+
+          <!--</li>-->
+          <!--<span>|</span>-->
+          <!--<li class="mainlevel">-->
+            <!--<a href="list_cp.html">产  品  介 绍</a>-->
+
+          <!--</li>-->
+          <!--<span>|</span>-->
+          <!--<li class="mainlevel">-->
+            <!--<a href="msg_case.html">施  工  工 艺</a>-->
+
+          <!--</li>-->
+          <!--<span>|</span>-->
+          <!--<li class="mainlevel">-->
+            <!--<a href="list_case.html">工  程  案 例</a>-->
+
+          <!--</li>-->
+          <!--<span>|</span>-->
+          <!--<li class="mainlevel li-last">-->
+            <!--<a href="lxwm.html">联系我们</a>-->
+
+          <!--</li>-->
+        <!--</ul>-->
+      <!--</div>-->
+    <!--</nav>-->
+  <!--</header>-->
+  <!-- 头部结束 -->
+
+
+  <!-- 手机版导航 -->
+  <div class="phone_nav hidden_lg">
+    <div class="headDiv">
+      <div class="headDivcon wrap">
+        <a href="javascript:;" class="logo" style="cursor: default;"><img src="images/logo1.png"></a>
+        <a href="javascript:;" class="navIco"><img src="images/navico.png"></a>
+      </div>  <!-- wal -->
+    </div>
+    <div class="navLayer" style="right: -100%; width: 0px;">
+      <div class="bg">
+        <a href="javascript:;" class="closeBtn"></a>
+        <div class="top">
+          <a href="javascript:;" class="logo"><img src="images/logo1.png"></a>
+        </div>
+        <div class="sideNav">
+          <div class="navContent">
+            <a href="javascript:;" class="back"><img src="images/nimg16_2.png"></a>
+            <ul>
+              <li><a href="index.html" target="_blank">走进中润泰兴新型建材有限公司</a></li>
+              <li><a href="onepage.html" target="_blank">公司简介</a></li>
+              <li><a href="list_cp.html" target="_blank">产品介绍</a></li>
+              <li><a href="msg_case.html" target="_blank">施工工艺</a></li>
+              <li><a href="list_case.html" target="_blank">工程案例</a></li>
+              <li><a href="lxwm.html" target="_blank">联系我们</a></li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- 手机版结束 -->
+
+  <!-- =======================以上为公共头部 ======================  -->
+
+
+  <!--<link href="images/css/index.css" rel="stylesheet" type="text/css">-->
+  <!--<script src="images/js/jquery.SuperSlide.2.1.1.js"></script>-->
+  <!-- pc轮播图开始-->
+  <div id="slideBox" class="slideBox1">
+    <div class="hd">
+      <ul></ul>
+    </div>
+    <div class="bd">
+      <ul>
+        <li><a href="javascript:;"><img src="images/qidian5_cn_5e9815a16b944179.jpg"  alt="banner" ></a></li>
+        <li><a href="javascript:;"><img src="images/1-1ZG5141GQ30.jpg"  alt="banner" ></a></li>
+        <li><a href="javascript:;"><img src="images/1-1ZFQG253X2.jpg"  alt="banner" ></a></li>
+      </ul>
+    </div>
+    <a class="prev" href="javascript:void(0)"></a>
+    <a class="next" href="javascript:void(0)"></a>
+  </div>
+  <!-- pc轮播图结束 -->
+
+
+  <!-- 手机版轮播图开始 -->
+  <div id="focus" class="focus">
+    <div class="hd">
+      <ul></ul>
+    </div>
+    <div class="bd">
+      <ul>
+        <li><a href="#"><img src="images/1-1P30F9224X56.jpg"  /></a></li>
+        <li><a href="#"><img src="images/1-1ZG5141GQ30.jpg" /></a></li>
+        <li><a href="#"><img src="images/1-1ZFQG253X2.jpg" /></a></li>
+      </ul>
+    </div>
+  </div>
+  <!--<script src="images/js/TouchSlide.1.1.js"></script>-->
+  <!-- pc轮播图结束 -->
+
+  <!-- 轮播图结束 -->
+
+  <!--
+  <div class="search">
+      <div class="wrap clearfix">
+          <div class="leftkeywords fl" >大家都在搜：<a href="javascript:;">隔音砂浆</a>|<a href="javascript:;">隔音砂浆配方</a> |<a href="javascript:;">隔音砂浆厂家</a>
+          <div class="search-form fr">
+              <form name="formsearch" action="">
+                  <input name="mid" value="1" type="hidden">
+                  <input name="dopost" value="search" type="hidden">
+                  <input name="q" class="keywords" placeholder="请输入关键字搜索" maxlength="20" type="text">
+                  <input name="submit" value="" class="search_btn abs" type="submit">
+              </form>
+          </div>
+      </div>
+  </div>
+
+  -->
+
+  <div class="news">
+    <div class="wrap clearfix">
+      <div style="margin-bottom:5px;">
+        <h1><strong>集团咨询</strong></h1>
+      </div>
+      <!--<div style="both:clear;margin-bottom:10px;" ></div>-->
+      <!--Start about us area-->
+      <section class="categories-areas" >   <!-- style="background-image:url(images/resources/categories-bg.jpg);"-->
+        <div class="container">
+          <div class="sec-title text-center">
+
+          </div>
+          <div class="row">
+            <div class="col-md-6">
+
+              <div class="about-content">
+                <img src = "images/gongshijianjie.png"  />
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <div class="choose-us-content">
+
+                <!--
+                <div class="button pull-right">
+                 <a href="#">更多新闻  <i class="fa fa-caret-right" aria-hidden="true"></i></a>
+            </div>
+             -->
+                <ul>
+                  <li>
+
+                    <div class="text-holders">
+                      <h4><a href="#"><span class="fa fa-globe"></span> 公司简介</a></h4>
+                      <p>深圳市中润泰兴新型建材有限公司从事建筑声学隔声系统产品的研发、生产和工程施工的高新技术企业通过不断的努力与科技创新，研发设计出一系列隔音、构造体系与相应的绿色环保隔音胶凝砂浆、隔音涂料、轻质墙板。公司致力于为大型房地产商、大型建筑总包商、政府部门提供高品质的隔音胶凝产品和工程施工服务，公司是目前国内隔音胶凝领域技术最优、规模最大、市场占有率最高的领先企业。
+                        公司以‘节能环保,绿色建筑’为时代责任，始终坚持以客户为根本的原则，为客户提供同行最先进、优质一体化的产品及服务，为绿色建筑节能事业再创辉煌！</p>
+                    </div>
+                  </li>
+
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!--End about us area-->
+
+    </div>
+  </div>
+
+  <!-- 媒体中心 -->
+  <div class="news">
+    <div class="wrap clearfix">
+      <div>
+        <h1><strong>产品咨询</strong></h1>
+      </div>
+      <div class="news-content clearfix">
+        <div class="news-left fl slideTxtBox">
+          <div class="switch-bar hd" style="height: auto;">
+            <ul id="move-animate-left clearfix">
+              <li>
+                <strong><a href="javascript:;">隔音胶凝砂浆材料</a></strong>
+              </li>
+              <li>
+                <strong><a href="javascript:;">隔音胶凝砂浆施工工艺</a></strong>
+              </li>
+              <li>
+                <strong> <a href="javascript:;">注意事项</a></strong>
+              </li>
+              <li>
+                <strong><a href="javascript:;">隔音涂料</a></strong>
+              </li>
+            </ul>
+          </div>
+          <div class="tabcon " id="leftcon">
+            <div class="subbox clearfix bd" >
+
+              <ul class="sublist">
+                <div class="media-body">
+                  <a href="javascript:;"><p class="name">材料组成</p></a>
+                  <p class="text">隔音胶凝砂浆主要由多孔助剂、吸音材料及天然环保型无机胶凝、增强纤维与外加组合而成的预混干粉砂浆。减振隔声砂浆中的阻尼材料调整了刚模量，增大机械能转化为热能的转化率而减弱高频传输，从而达到良好的隔音效果。</p>
+                  <a href="javascript:;"><p class="name">相关行业技术规范</p></a>
+                  <p class="text">目前我国普通住宅楼板隔声的技术标准《绿色建筑评价标准》4.5.3：分户楼板撞击声低限标准二级(计权标准化撞击声L＇nT,W≤75dB)，高要求住宅分户楼板撞击声隔音标准一级(计权标准化撞击声L＇nT,W≤65dB)。</p>
+                  <a href="javascript:;"><p class="name">产品特性</p></a>
+                  <p class="text">隔音胶凝砂浆与其他同类产品相比，隔音胶凝砂浆采用的是环保型新型材料，具有比重小，质地轻，减少建筑承载压力，且隔音性能好，粘接强度高，无味、无毒天然材料，无论在弹性、阻尼性还是使用寿命上均大大优于其他材料。</p>
+                </div>
+              </ul>
+
+
+              <ul class="sublist">
+                <div class="media-body">
+                  <a href="javascript:;"><p class="name">技术资料</p></a>
+                  <p class="text"><img src="images/gyxj_jszl1.png"/></p>
+                  <a href="javascript:;"><p class="name">施工工艺</p></a>
+                  <p class="text">·基层处理--地面清洗--弹线--打灰饼、冲筋--搅拌、平铺隔音胶凝。</p>
+                  <p class="text">·涂刷界面剂：喷涂一遍专用界面剂，也可选择滚涂进一步增强与基面的粘结性能；</p>
+                  <p class="text">·搅拌隔音胶凝：采用机械搅拌，投料顺序为隔音胶凝、水，质量比为 1: 0.7~0.8 ；</p>
+                  <p class="text">·铺设隔音胶凝：清洗完地面后，搅拌均匀隔音胶凝砂浆后将隔音胶泥铺匀，然后用木刮杠按灰饼（冲筋）高度刮平，再用木抹子抹平，从内向外退着操作，也可以用喷涂方式铺设隔音胶凝；</p>
+                </div>
+              </ul>
+
+
+              <ul class="sublist">
+                <div class="media-body">
+                  <a href="javascript:;"><p class="name">注意事项</p></a>
+                  <p class="text">·养护：施工完毕后，宜在通风干燥环境中养护15 天以上为宜，整体未完全干燥达到强度前，禁止承受重载。</p>
+                  <p class="text">·彻底清洁基层，无粘结物、油污、灰尘，清洗润湿基面，结合更紧密，施工期间注意表面平整度，2m 内平整度应达到 2mm，防止空鼓、裂缝；</p>
+                  <p class="text">·基层应有一定强度，大于 20MPa 为宜，若是新浇筑的混凝土地面应养护 28天以上，再进行施工；</p>
+                  <p class="text">·切忌将已干结的浆料再加水混合后使用；</p>
+                  <p class="text">·封闭现场施工，避免阳光直射和人员踩踏，施工温度 5℃~40℃为宜；养护期间，避免人行走和承受重载，养护至相应龄期为宜。</p>
+                  <p class="text">·保存：存放于阴凉干燥处，产品储存期为一年；包装：25 公斤/袋。</p>
+                </div>
+              </ul>
+
+
+              <ul class="sublist">
+                <div class="media-body">
+                  <a href="javascript:;"><p class="name">产品简介</p></a>
+                  <p class="text">1、施工简单方便:比传统方法减少50%以上的工序。 涂料K81，耐候性能好,附着力好,水性环保,降噪隔音系数 接近隔音棉(同厚度)的效果,可满足隔音、保温的要求。</p>
+                  <p class="text">2、相对超薄：通常总厚度3~8毫米，节省空间。</p>
+                  <p class="text">3、隔音涂料可以做到A1级防火,环保,无污染,对建筑起到良好保护作用。</p>
+                  <p class="text"> 4、隔音阻尼减震性能好,优于同等厚度其它隔音材料。</p>
+                  <p class="text">5、附着力好,渗透力好,和基材相容性好,不会出现开裂。</p>
+                  <p class="text">6、对建筑结构保护好,不易引起热桥,温差,声桥。</p>
+                </div>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="news-right fr">
+          <div class="pictureContainer slideImgBox" id="pictureContainer">
+            <div class="sildeimg bd">
+              <ul class="slideshow" id="slidesImgs">
+                <li><a href="javascript:;"><img src="images/ctxx_geyincailiao.png" alt="隔音胶凝砂浆材料" width="100%" border="0"></a><span><a href="javascript:;">隔音胶凝砂浆材料</a></span></li>
+                <li><a href="javascript:;"><img src="images/jtzc_geyintuliao1.png" alt="隔音涂料介绍" width="100%" border="0"></a><span><a href="javascript:;">隔音涂料介绍</a></span></li>
+                <li><a href="javascript:;"><img src="images/jtzc_geyinbaan1.png" alt="轻质隔墙板介绍" width="100%" border="0"></a><span><a href="javascript:;">轻质隔墙板介绍</a></span></li>
+                <li><a href="javascript:;"><img src="images/jtzc_qianban1.png" alt="墙板安装辅材" width="100%" border="0"></a><span><a href="javascript:;">墙板安装辅材</a></span></li>
+              </ul>
+            </div>
+            <div class="slidebar " id="slideBar">
+              <ul class="hd clearfix">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+            <a class="more" href="javascript:;" title="更多资讯"></a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+    <!--<script src="images/js/index.js"></script>-->
+   <!-- ===============================以下为公共底部 ======================-->
+  <!--footer start-->
+  <!--<footer class="footer">-->
+    <!--<div class="footer_top wrap clearfix">-->
+      <!--&lt;!&ndash;  <a href="javascript:;" class="friend-ico"><img src="images/footer_logo1.jpg" alt=""></a>  &ndash;&gt;-->
+      <!--<a href="javascript:;" class="friend-ico"><img src="images/footer_logo2.jpg" alt=""></a>-->
+      <!--<div class="top_right fr">-->
+        <!--<ul>-->
+          <!--<li><a href="javascript:;">OA系统</a></li>-->
+          <!--<span>|</span>-->
+          <!--<li><a href="javascript:;">法律声明</a></li>-->
+          <!--<span>|</span>-->
+          <!--<li><a href="javascript:;">隐私保护</a></li>-->
+          <!--<span>|</span>-->
+          <!--<li><a href="javascript:;">联系我们</a></li>-->
+          <!--<span>|</span>-->
+          <!--<li><a href="javascript:;">站长统计</a></li>-->
+          <!--<span>|</span>-->
+          <!--<li><a href="javascript:;">360安全联盟</a></li>-->
+        <!--</ul>-->
+        <!--<p>版权所有 <a href="http://www.beian.miit.gov.cn">粤ICP备19018833号-1</a></p>-->
+      <!--</div>-->
+    <!--</div>-->
+    <!--<div class="footer_bottom">-->
+      <!--<div class="wrap clearfix">-->
+        <!--<div class="bottom_left">-->
+          <!--<div class="footer_logo table_box">-->
+            <!--<div class="table_cell">-->
+              <!--<img src="images/logo1.png" alt=""/>-->
+            <!--</div>-->
+          <!--</div>-->
+
+          <!--<div class="company_info">-->
+            <!--中润泰兴新型建材有限公司<br>-->
+            <!--联系人：彭先生<br>-->
+            <!--电话：15012652898  13530896586 <br>-->
+            <!--邮箱：408123258@qq.com<br>-->
+            <!--网址：<a href ="http://www.kingzao.cn/h5Index/" style="color:#ffffff;">http://www.kingzao.cn/h5Index/</a><br>-->
+            <!--地址：深圳市坪山新区坑梓街道乌石路46号   <br>-->
+          <!--</div>-->
+        <!--</div>-->
+        <!--<div class="bottom_right">-->
+          <!--&lt;!&ndash;-->
+                      <!--<div class="imgbox">-->
+                          <!--<img src="images/ma1.jpg" alt="" />-->
+                          <!--<p>扫码访问手机版</p>-->
+                      <!--</div>-->
+                      <!--<div class="imgbox">-->
+                          <!--<img src="images/ma1.jpg" alt="" />-->
+                          <!--<p>扫码访问手机版</p>-->
+                      <!--</div>-->
+                          <!--&ndash;&gt;-->
+        <!--</div>-->
+
+      <!--</div>-->
+    <!--</div>-->
+  <!--</footer>-->
+
+
+  <!--公共侧边栏-->
+  <div class="toolbar" style="background-color:#4C8FB6;padding:10px 5px;" >
+    <a href="http://www.kingzao.cn/h5Index/lxwm.html" style="display:block; color:#FFFFFF;">
+      <!--<a href="javascript:scroll(0,0)" id="top" class="toolbar-item toolbar-item-top">111111</a>-->
+      <!--<a href="tencent://message/?uin=285772998&Site=企业网站&Menu=yes" class="toolbar-item toolbar-item-feedback">222222</a>-->
+      <!--<a href="http://www.kingzao.cn/h5Index/lxwm.html" class="toolbar-item toolbar-item-app"><span class="toolbar-layer"></span></a>-->
+      <p>联系电话：<br/>
+        15012652898<br/>
+        13530896586
+      </p>
+
+      <!--<img src="images/fix_hover.png" />-->
+    </a>
+  </div>
+  <!--公共侧边栏 end-->
+</div>
+</template>
+
+<script>
+export default{
+  name: 'index',
+};
+</script>
+
+<style scoped>
+
+</style>
